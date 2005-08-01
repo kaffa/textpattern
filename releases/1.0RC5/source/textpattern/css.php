6b33110da2a15c4eafb8a6bfbe8ff3464bc89ff7
sencer <sencer@2fea6a4d-a838-0410-917b-93a53c48e9d2>
2005-08-01 10:53:43 +0000

    Tag: RC5. This is RC5.

---
+++ releases/1.0RC5/source/textpattern/css.php
@@ -0,0 +1,14 @@
+<?php
+/*
+$HeadURL$
+$LastChangedRevision$
+*/
+
+header('Content-type: text/css');
+include './config.php';
+$nolog = 1;
+include $txpcfg['txpath'].'/publish.php';
+$s = gps('s');
+$n = gps('n');
+output_css($s,$n);
+?>
