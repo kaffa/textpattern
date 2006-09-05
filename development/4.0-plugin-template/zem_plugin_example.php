dc5c415236a7d59d4da64e5acf4b80b84f987b59
zem <zem@2fea6a4d-a838-0410-917b-93a53c48e9d2>
2006-09-05 23:20:53 +0000

    fix line ends; change comments

--- development/4.0-plugin-template/zem_plugin_example.php
+++ development/4.0-plugin-template/zem_plugin_example.php
@@ -9,8 +9,7 @@
 // Plugin name is optional.  If unset, it will be extracted from the current
 // file name. Uncomment and edit this line to override:
 # $plugin['name'] = 'abc_plugin';
-# $plugin['compress'] = 1; // only works for 4.0.3 or later; only when zlib is enabled
-# $plugin['allow_html_help'] = 1 // Use only when you want to also use html in your help-text
+# $plugin['allow_html_help'] = 1 // Allow raw HTML help, as opposed to Textile.  Not recommended.
 
 $plugin['version'] = '0.1';
 $plugin['author'] = 'Threshold State';
