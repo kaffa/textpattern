5154896795bb385a481d7e32c6859b8ff182ed40
r.wetzlmayr <r.wetzlmayr@2fea6a4d-a838-0410-917b-93a53c48e9d2>
2012-12-05 08:55:43 +0000

    This is Textpattern 4.5.4

--- releases/4.5.4/source/textpattern/update/_update.php
+++ releases/4.5.4/source/textpattern/update/_update.php
@@ -118,10 +118,10 @@ $LastChangedRevision$
 			$dbversion = '4.4.1';
 	}
 
-	if (version_compare($dbversion, '4.5.3', '<'))
+	if (version_compare($dbversion, '4.5.4', '<'))
 	{
 		if ((include txpath.DS.'update'.DS.'_to_4.5.0.php') !== false)
-			$dbversion = '4.5.3';
+			$dbversion = '4.5.4';
 	}
 
 	// Invite optional third parties to the update experience
