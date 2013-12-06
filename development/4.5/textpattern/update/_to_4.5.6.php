39c1d3050a15e55ad490d1bccde644427b1e459d
jukka.m.svahn@gmail.com <jukka.m.svahn@gmail.com@2fea6a4d-a838-0410-917b-93a53c48e9d2>
2013-12-06 12:46:14 +0000

    Fit more than just IPv4 addresses to visitor logs.
    r4330 for 4.5, see issue #290

--- development/4.5/textpattern/update/_to_4.5.6.php
+++ development/4.5/textpattern/update/_to_4.5.6.php
@@ -7,3 +7,6 @@
 
 	// Updates comment email length.
 	safe_alter('txp_discuss', "MODIFY email VARCHAR(254) NOT NULL default ''");
+
+	// Store IPv6 properly in logs.
+	safe_alter('txp_log', "MODIFY ip VARCHAR(45) NOT NULL default ''");
