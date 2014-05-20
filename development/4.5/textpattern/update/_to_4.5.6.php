540f9efeaa9459d173a23b04be6fa5ced92dc5ce
stefdawson <stefdawson@2fea6a4d-a838-0410-917b-93a53c48e9d2>
2014-05-20 22:52:49 +0000

    Ensure database fields have default values. See Issue 410.

--- development/4.5/textpattern/update/_to_4.5.6.php
+++ development/4.5/textpattern/update/_to_4.5.6.php
@@ -13,3 +13,10 @@
 
 	// Save sections correctly in articles.
 	safe_alter('textpattern', "MODIFY Section VARCHAR(128) NOT NULL default ''");
+
+	// Ensure all memory-mappable columns have defaults
+	safe_alter('txp_form', "MODIFY `name` VARCHAR(64) NOT NULL default ''");
+	safe_alter('txp_page', "MODIFY `name` VARCHAR(128) NOT NULL default ''");
+	safe_alter('txp_prefs', "MODIFY `prefs_id` INT(11) NOT NULL default '1'");
+	safe_alter('txp_prefs', "MODIFY `name` VARCHAR(255) NOT NULL default ''");
+	safe_alter('txp_section', "MODIFY `name` VARCHAR(128) NOT NULL default ''");
